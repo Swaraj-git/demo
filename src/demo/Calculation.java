f7759a2583124c24fc1bde4e974e5e895a702d29
DESKTOP-4B8560R\Swaraj Singh Tomar <swaraj.tomar@nitorinfotech.com>
2020-08-12 18:30:19 +0530

    Commiting untracked changes

---
+++ src/demo/Calculation.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * Class contains methods for calculation
+ * 
+ * @author Swaraj Singh Tomar
+ *
+ */
+public class Calculation {
+
+	public static void main(String[] args) {
+		System.out.println("This is a calculation class");
+	}
+
+}
